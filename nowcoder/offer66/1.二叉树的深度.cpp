09eff6eb8c4c07dd2e3b96fb3432efc030180275
HaxiSnake <haxisnake@sina.com>
2019-05-29 11:28:37 +0800

    nowcoder

---
+++ nowcoder/offer66/1.二叉树的深度.cpp
@@ -0,0 +1,20 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(pRoot==NULL){
+            return 0;
+        }else{
+            return 1+max(TreeDepth(pRoot->left),TreeDepth(pRoot->right));
+        }
+    }
+};
