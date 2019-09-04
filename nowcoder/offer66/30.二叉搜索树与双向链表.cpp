c868d398b2d6fb5499f54b5880610541cae5872c
HaxiSnake <haxisnake@sina.com>
2019-09-04 10:27:40 +0800

    offer66

--- nowcoder/offer66/30.二叉搜索树与双向链表.cpp
+++ nowcoder/offer66/30.二叉搜索树与双向链表.cpp
@@ -1,9 +1,4 @@
-import os
-os.chdir(os.path.dirname(__file__))
-title="30.二叉搜索树与双向链表"
-content="输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。"
-isstar=True
-code="""
+
 /*
 struct TreeNode {
     int val;
@@ -36,18 +31,3 @@ public:
         convertHelper(cur ->right, pre);
     }
 };
-"""
-md_list="* [{0}](#{0}) [解答](./{0}.cpp)".format(title)
-essay="## {0}\n\n{1}\n".format(title,content)
-if(isstar):
-    md_list+=" &star;"
-
-print(md_list)
-print(essay)
-
-with open("{0}.cpp".format(title),'w') as out:
-    out.writelines(code)
-out.close()
-
-
-
