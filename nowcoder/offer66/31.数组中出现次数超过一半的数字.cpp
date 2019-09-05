719362eaf749655f2c3125facb7cb46c6af71a75
HaxiSnake <haxisnake@sina.com>
2019-09-05 09:59:13 +0800

    offer66

--- nowcoder/offer66/31.数组中出现次数超过一半的数字.cpp
+++ nowcoder/offer66/31.数组中出现次数超过一半的数字.cpp
@@ -1,9 +1,4 @@
-import os
-os.chdir(os.path.dirname(__file__))
-title="31.数组中出现次数超过一半的数字"
-content="数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0."
-isstar=True
-code="""
+
 public:
     int MoreThanHalfNum_Solution(vector<int> numbers) {
         int len = numbers.size();
@@ -34,18 +29,3 @@ public:
         return count>len/2?value:0;        
     }
 };
-"""
-md_list="* [{0}](#{0}) [解答](./{0}.cpp)".format(title)
-essay="## {0}\n\n{1}\n".format(title,content)
-if(isstar):
-    md_list+=" &star;"
-
-print(md_list)
-print(essay)
-
-with open("{0}.cpp".format(title),'w') as out:
-    out.writelines(code)
-out.close()
-
-
-
