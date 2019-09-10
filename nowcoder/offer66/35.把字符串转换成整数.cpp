1ad50df3c31e550a335404303ffbe8d637cdac27
HaxiSnake <haxisnake@sina.com>
2019-09-10 11:21:32 +0800

    offer66

--- nowcoder/offer66/35.把字符串转换成整数.cpp
+++ nowcoder/offer66/35.把字符串转换成整数.cpp
@@ -1,9 +1,4 @@
-import os
-os.chdir(os.path.dirname(__file__))
-title="35.把字符串转换成整数"
-content="将一个字符串转换成一个整数(实现Integer.valueOf(string)的功能，但是string不符合数字要求时返回0)，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0。"
-isstar=True
-code="""
+
 class Solution {
 public:
     int StrToInt(string str) {
@@ -37,18 +32,3 @@ public:
         return flag*res;
     }
 };
-"""
-md_list="* [{0}](#{0}) [解答](./{0}.cpp)".format(title)
-essay="## {0}\n\n{1}\n".format(title,content)
-if(isstar):
-    md_list+=" &star;"
-
-print(md_list)
-print(essay)
-
-with open("{0}.cpp".format(title),'w') as out:
-    out.writelines(code)
-out.close()
-
-
-
